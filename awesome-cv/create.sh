74f1e00eb534ecee63a8c47c5c125855c2a247f8
Jeffrey Hafner <jeffrey.p.hafner@gmail.com>
2024-12-18 18:33:00 -0500

    updated resume

--- awesome-cv/create.sh
+++ awesome-cv/create.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-colors=(emerald skyblue red pink orange nephritis concrete darknight)
-#colors=(emerald)
+#colors=(emerald skyblue red pink orange nephritis concrete darknight)
+colors=(emerald)
 
 
 for color in ${colors[@]}; do
   sed "s/COLOR/${color}/" resume.tex > resume-${color}.tex
-  latexmk -quiet -lualatex -f ./resume-${color}.tex
-  #latexmk -lualatex -f ./resume-${color}.tex
+  latexmk -lualatex -f ./resume-${color}.tex
 done
 
-
 latexmk -c
-
 
 for color in ${colors[@]}; do
     rm -f resume-${color}.tex
